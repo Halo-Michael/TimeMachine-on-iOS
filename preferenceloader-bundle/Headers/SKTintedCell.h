591599d2c0748f448910ac42cfbae288f0aed1d4
Halo-Michael <45062120+Halo-Michael@users.noreply.github.com>
2020-03-27 02:41:00 +0800

    Add files via upload

--- preferenceloader-bundle/Headers/SKTintedCell.h
+++ preferenceloader-bundle/Headers/SKTintedCell.h
@@ -1,4 +1,18 @@
-#import <Preferences/Preferences.h>
+#import <Preferences/PSBundleController.h>
+#import <Preferences/PSControlTableCell.h>
+#import <Preferences/PSDiscreteSlider.h>
+#import <Preferences/PSEditableTableCell.h>
+#import <Preferences/PSHeaderFooterView.h>
+#import <Preferences/PSListController.h>
+#import <Preferences/PSListItemsController.h>
+#import <Preferences/PSRootController.h>
+#import <Preferences/PSSliderTableCell.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSSwitchTableCell.h>
+#import <Preferences/PSSystemPolicyForApp.h>
+#import <Preferences/PSTableCell.h>
+#import <Preferences/PSViewController.h>
+#import <Preferences/PreferencesAppController.h>
 
 @interface SKTintedCell : PSTableCell
 @end
